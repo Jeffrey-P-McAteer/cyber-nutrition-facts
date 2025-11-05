d72bebdedf8f0fa7664f33771cb3292425d3b5c1
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2025-11-05 08:02:12 -0500

    --style

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,21 @@
 
 #[derive(Debug, clap::Parser)]
+#[command(
+    name = "cyber-nutrition-facts",
+    version = "1.0",
+    about = "A source-code, executable-binary, and web-url security information gathering and reporting utility."
+)]
 pub struct Args {
+    /// Data to be analyzed. Supported types of data are: Single Source-Code file, Single PE32, PE32+ .exe binary, single ELF binary, and a web HTTP/S URL.
     pub input: AnalysisInput,
 
+    /// File path where the output .pdf report will be placed. If none is provided, data is written to STDOUT.
     #[arg(short, long)]
     pub output_report: Option<std::path::PathBuf>,
+
+    /// Report Style. Valid ReportStyles are [t|terse, n|normal, d|detailed, o|overflowing]. Pass "--style help" to list all options.
+    #[arg(short, long, default_value = "normal")]
+    pub style: ReportStyle,
 }
 
 #[derive(Debug, Clone)]
@@ -115,6 +126,76 @@ impl std::convert::From<&std::path::Path> for AnalysisInput {
         else {
             // Assume non-existent file
             AnalysisInput::File(value.to_path_buf())
+        }
+    }
+}
+
+
+#[derive(Debug, Clone)]
+pub enum ReportStyle {
+    Terse,
+    Normal,
+    Detailed,
+    Overflowing,
+}
+
+impl ReportStyle {
+    pub fn description(&self) -> String {
+        match self {
+            ReportStyle::Terse => "Only the top-level summary for each type of information available is printed. Missing types of data are omitted entirely.".into(),
+            ReportStyle::Normal => "Everything in Terse are printed, plus MISSING/UNKNOWN for unavailable data. Additionally one level under each top-level summary is printed.".into(),
+            ReportStyle::Detailed => "Everything in Normal plus parentheticals, sources of data, and other partially-summarized details which we can not algorithmically decide and are generally weighted low or discarded during summarization.".into(),
+            ReportStyle::Overflowing => "Everything we see, before any summarization processing takes place.".into(),
+        }
+    }
+    pub fn named_description(&self) -> String {
+        match self {
+            ReportStyle::Terse => format!("t|terse: {}", self.description()),
+            ReportStyle::Normal => format!("n|normal: {}", self.description()),
+            ReportStyle::Detailed => format!("d|detailed: {}", self.description()),
+            ReportStyle::Overflowing => format!("o|overflowing: {}", self.description()),
+        }
+    }
+}
+
+impl std::str::FromStr for ReportStyle {
+    type Err = String;
+
+    #[allow(irrefutable_let_patterns)]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.to_lowercase();
+        if s == "t" || s == "terse" {
+            Ok(ReportStyle::Terse)
+        }
+        else if s == "n" || s == "normal" {
+            Ok(ReportStyle::Normal)
+        }
+        else if s == "d" || s == "detailed" {
+            Ok(ReportStyle::Detailed)
+        }
+        else if s == "o" || s == "overflowing" || s == "overflow" {
+            Ok(ReportStyle::Overflowing)
+        }
+        else {
+            let mut style_detail_lines = String::with_capacity(2048);
+
+            style_detail_lines.push_str("   - ");
+            style_detail_lines.push_str(ReportStyle::Terse.named_description().as_str());
+            style_detail_lines.push_str("\n");
+            style_detail_lines.push_str("   - ");
+
+            style_detail_lines.push_str(ReportStyle::Normal.named_description().as_str());
+            style_detail_lines.push_str("\n");
+
+            style_detail_lines.push_str("   - ");
+            style_detail_lines.push_str(ReportStyle::Detailed.named_description().as_str());
+            style_detail_lines.push_str("\n");
+
+            style_detail_lines.push_str("   - ");
+            style_detail_lines.push_str(ReportStyle::Overflowing.named_description().as_str());
+            // style_detail_lines.push_str("\n");
+
+            Err(format!("Unknown ReportStyle: '{s}'.\nValid ReportStyles are [t|terse, n|normal, d|detailed, o|overflowing]:\n{}", style_detail_lines))
         }
     }
 }
