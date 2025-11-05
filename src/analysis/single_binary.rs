f10c2a0de65a1dd40e6188e9a5f6b90ca7a21dbf
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-11-05 16:37:23 -0500

    data!

--- src/analysis/single_binary.rs
+++ src/analysis/single_binary.rs
@@ -1,7 +1,17 @@
 
 pub fn analyze_single_binary(path: &std::path::Path, args: &crate::args::Args) -> crate::DynResult<()> {
     
-    println!("TODO analyze_single_binary {:?}", path);
+    let binary_content_bytes = std::fs::read(path)?;
+
+    let obj = goblin::Object::parse(&binary_content_bytes)?;
+
+    if args.style >= crate::args::ReportStyle::Detailed {
+        println!("TODO analyze_single_binary {:?}", path);
+        println!("obj = {:?}", obj);
+    }
+    else {
+        println!("TODO pass '--style detailed' or greater for in-development outputs.")
+    }
 
     Ok(())
 }
