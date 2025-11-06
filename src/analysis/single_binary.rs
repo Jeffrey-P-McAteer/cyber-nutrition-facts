0ab45679e087b7a639c39d8df0be149a2be9fb4a
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-11-05 19:44:28 -0500

    func names

--- src/analysis/single_binary.rs
+++ src/analysis/single_binary.rs
@@ -22,7 +22,9 @@ pub fn print_referenced_libraries(prefix: &str, gobj: &goblin::Object, args: &cr
     match gobj {
         goblin::Object::Elf(elf) => {
             println!("{}= = = = Shared Libraries = = = =", prefix);
+
             let dynamic_libs = elf.dynamic.as_ref().map(|v| v.get_libraries(&elf.dynstrtab)).unwrap_or_else(|| vec![]);
+
             if dynamic_libs.len() < 1 {
                 println!("{}NO LIBRARIES REFERENCED IN elf.dynstrtab", prefix);
             }
@@ -35,13 +37,33 @@ pub fn print_referenced_libraries(prefix: &str, gobj: &goblin::Object, args: &cr
         }
         goblin::Object::PE(pe) => {
             println!("{}= = = = Shared Libraries = = = =", prefix);
+
             let import_libs: Vec<String> = pe.import_data.as_ref().map(|v| (&v.import_data).into_iter().map(|id| id.name.to_string()).collect() ).unwrap_or_else(|| vec![]);
+            let mut lib_funcs: std::collections::HashMap<String, Vec<String>> = std::collections::HashMap::new();
+
+            if let Some(import_datas) = &pe.import_data {
+                for import_data in import_datas.import_data.iter() {
+                    let mut funcs = Vec::new();
+                    if let Some(ilt_vec) = &import_data.import_lookup_table {
+                        for ilt in ilt_vec.iter() {
+                            funcs.push(silte_to_string(ilt));
+                        }
+                    }
+                    lib_funcs.insert(import_data.name.to_string(), funcs);
+                }
+            }
+
             if import_libs.len() < 1 {
                 println!("{}NO LIBRARIES REFERENCED IN pe.import_data", prefix);
             }
             else {
                 for lib in import_libs.iter() {
                     println!("{} - {}", prefix, lib);
+                    if let Some(funcs) = lib_funcs.get(lib) {
+                        for func in funcs.iter() {
+                            println!("{}   - {}", prefix, func);
+                        }
+                    }
                 }
             }
         }
@@ -50,4 +72,16 @@ pub fn print_referenced_libraries(prefix: &str, gobj: &goblin::Object, args: &cr
         }
     }
 }
+
+fn silte_to_string(silte: &goblin::pe::import::SyntheticImportLookupTableEntry) -> String {
+    match silte {
+        goblin::pe::import::SyntheticImportLookupTableEntry::OrdinalNumber(num) => {
+            format!("{} (Only an OrdinalNumber)", num)
+        }
+        goblin::pe::import::SyntheticImportLookupTableEntry::HintNameTableRVA((num, table_entry)) => {
+            format!("{} ({})", table_entry.name, num)
+        }
+    }
+}
+
 
