53c90ce258b969b1ffaa4a7d14c2445ef7600ffc
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-11-06 07:31:46 -0500

    misc

--- samples/c_safe_a.c
+++ samples/c_safe_a.c
@@ -1,9 +1,19 @@
 
 #include <stdio.h>
 
+void do_some_stuff(char* name, int length) {
+    printf("Hello, %.*s!\n", length, name);
+}
+
 int main(int argc, char** argv) {
 
     printf("Hello, Safe C!\n");
 
+    char* name = "Test";
+    do_some_stuff(name, 4);
+
     return 0;
 }
+
+
+
--- samples/c_unsafe_a.c
+++ samples/c_unsafe_a.c
@@ -1,9 +1,15 @@
 
 #include <stdio.h>
 
+void do_some_stuff(char* name) {
+    printf("Hello, %s!\n", name);
+}
+
 int main(int argc, char** argv) {
     
     printf("Hello, Unsafe %s!\n", argv[1]); // OOB read if ARG1 not passed
+
+    do_some_stuff(argv[2]);
 
     return 0;
 }
